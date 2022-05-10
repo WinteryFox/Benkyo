4639f8931fec0dbe302d9d1c9fca2e9acde5bd1f
AmyFoxie <dragonhel5@gmail.com>
2022-05-10 23:28:07 +0200

    make fields for deck patch request nullable

--- decks/src/main/kotlin/com/benkyo/decks/controller/DeckController.kt
+++ decks/src/main/kotlin/com/benkyo/decks/controller/DeckController.kt
@@ -7,6 +7,7 @@ import com.benkyo.decks.exceptions.UnauthorizedException
 import com.benkyo.decks.repository.DeckRepository
 import com.benkyo.decks.repository.UserRepository
 import com.benkyo.decks.request.DeckCreateRequest
+import com.benkyo.decks.request.DeckPatchRequest
 import com.benkyo.decks.utils.isValidLocaleCode
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
@@ -65,35 +66,30 @@ class DeckController(
         exchange: ServerWebExchange,
         principal: Principal,
         @PathVariable id: String,
-        @Valid @RequestBody request: DeckCreateRequest
+        @Valid @RequestBody request: DeckPatchRequest
     ): Deck? {
         val user = userRepository.findById(principal.name)
             ?: throw UnauthorizedException()
 
         val deck = deckRepository.findById(id)
 
-        if (deck == null || deck.author != user.id) {
-            // TODO: See TODO in CardController about status codes
-
+        // TODO: See TODO in CardController about status codes
+        if (deck == null || deck.author != user.id)
             throw DeckNotFoundException(id)
-        }
 
-        if (!request.sourceLanguage.isValidLocaleCode()) {
+        if (request.sourceLanguage != null && !request.sourceLanguage.isValidLocaleCode())
             throw InvalidLocaleException(request.sourceLanguage)
-        }
-
-        if (!request.targetLanguage.isValidLocaleCode()) {
+        if (request.targetLanguage != null && !request.targetLanguage.isValidLocaleCode())
             throw InvalidLocaleException(request.targetLanguage)
-        }
 
         return deckRepository.save(
             deck.copy(
-                description = request.description,
-                isPrivate = request.isPrivate,
-                name = request.name,
-                shortDescription = request.shortDescription,
-                sourceLanguage = request.sourceLanguage,
-                targetLanguage = request.targetLanguage
+                description = request.description ?: deck.description,
+                isPrivate = request.isPrivate ?: deck.isPrivate,
+                name = request.name ?: deck.name,
+                shortDescription = request.shortDescription ?: deck.shortDescription,
+                sourceLanguage = request.sourceLanguage ?: deck.sourceLanguage,
+                targetLanguage = request.targetLanguage ?: deck.targetLanguage
             )
         )
     }
--- decks/src/main/kotlin/com/benkyo/decks/request/DeckRequest.kt
+++ decks/src/main/kotlin/com/benkyo/decks/request/DeckRequest.kt
@@ -21,3 +21,17 @@ data class DeckCreateRequest(
     @field:Size(min = 5, max = 5)
     val targetLanguage: String
 )
+
+data class DeckPatchRequest(
+    @field:Size(min = 4, max = 64)
+    val name: String? = null,
+    val isPrivate: Boolean? = null,
+    @field:Size(min = 4, max = 128)
+    val shortDescription: String? = null,
+    @field:Size(min = 4, max = 2048)
+    val description: String? = null,
+    @field:Size(min = 5, max = 5)
+    val sourceLanguage: String? = null,
+    @field:Size(min = 5, max = 5)
+    val targetLanguage: String? = null
+)
